1d7939dbfc63786d2fc1d752c3a21df138dd4e59
bartodziej777 <bartek3993@gmail.com>
2023-05-24 14:20:07 +0200

    firebase initialization

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXofkkCdRO5EskMGM14D6UTfbW6797azI",
+  authDomain: "money-monitor-83522.firebaseapp.com",
+  projectId: "money-monitor-83522",
+  storageBucket: "money-monitor-83522.appspot.com",
+  messagingSenderId: "156929048427",
+  appId: "1:156929048427:web:2711aea5d4ad3d08ddd62e",
+  measurementId: "G-JBRDWS1TJD",
+};
+
+const app = initializeApp(firebaseConfig);
