78efcd2d2a21f5a8728cd80e1d21e94575d7f1d8
bartodziej777 <bartek3993@gmail.com>
2023-05-25 00:53:12 +0200

    store created

---
+++ src/store/slices/themesSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = "light";
+
+export const themesSlice = createSlice({
+  name: "themes",
+  initialState,
+  reducers: {},
+});
