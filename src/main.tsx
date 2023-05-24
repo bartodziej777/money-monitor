2fc2041fc2bf80b5a5ed844ff3c0151d004007b2
bartodziej777 <bartek3993@gmail.com>
2023-05-24 23:11:51 +0200

    creating global css styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import GlobalCSS from "./global.style.ts";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalCSS />
       <App />
     </BrowserRouter>
   </React.StrictMode>
