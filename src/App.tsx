62386f6b8845ed31b9b0ce968a6fc4a01cdb8460
bartodziej777 <bartek3993@gmail.com>
2023-05-25 15:19:39 +0200

    implement theme setting based on state from redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import type { RootState } from "./store/store";
+import { useSelector } from "react-redux/es/exports";
 import Routes from "./routes/Routes";
+import { darkMode, lightMode } from "./styles/themes.style";
 
 function App() {
-  return <Routes />;
+  const theme = useSelector((state: RootState) => state.themes.mode);
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightMode : darkMode}>
+      <Routes />;
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import GlobalCSS from "./global.style.ts";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalCSS />
-      <App />
+      <Provider store={store}>
+        <GlobalCSS />
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
