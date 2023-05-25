4c5984f666b5c7397a04747c8f98997d1f46e942
bartodziej777 <bartek3993@gmail.com>
2023-05-25 14:52:01 +0200

    creating slice to dark mode

--- src/store/slices/themesSlice.ts
+++ src/store/slices/themesSlice.ts
@@ -1,9 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = "light";
+interface ThemesState {
+  mode: "light" | "dark";
+}
+
+const initialState: ThemesState = { mode: "light" };
 
 export const themesSlice = createSlice({
   name: "themes",
   initialState,
-  reducers: {},
+  reducers: {
+    changeTheme: (state) => {
+      if (state.mode === "light") state.mode = "dark";
+      else state.mode = "light";
+    },
+  },
 });
+
+export const { changeTheme } = themesSlice.actions;
+export default themesSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import themesReducer from "./slices/themesSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    themes: themesReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
