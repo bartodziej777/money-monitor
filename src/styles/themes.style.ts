51b43c40717688eb30e228396b12a757f2ab9d79
bartodziej777 <bartek3993@gmail.com>
2023-05-30 13:19:56 +0200

    set color pallete for project

---
+++ src/styles/themes.style.ts
@@ -0,0 +1,29 @@
+export const lightMode = {
+  primary: "#00ADB5",
+  primaryVariant: "#00949c",
+  secondary: "#FF2E63",
+  secondaryVariant: "#ff2e85",
+  background: "#eee",
+  surface: "#fff",
+  error: "#b00020",
+  onPrimary: "#fff",
+  onSecondary: "#000",
+  onBackground: "#000",
+  onSurface: "#000",
+  onError: "#fff",
+};
+
+export const darkMode = {
+  primary: "#00ADB5",
+  primaryVariant: "#00949c",
+  secondary: "#FF2E63",
+  secondaryVariant: "#ff2e85",
+  background: "#222831",
+  surface: "#393E46",
+  error: "#cf6679",
+  onPrimary: "#000",
+  onSecondary: "#000",
+  onBackground: "#eee",
+  onSurface: "#eee",
+  onError: "#000",
+};
