4cc59223fe326209dea317c1b88acf071b94c64a
bartodziej777 <bartek3993@gmail.com>
2023-07-03 23:10:46 +0200

    add login validation and redirecting after succesfull login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import {
   Form as StyledForm,
@@ -7,28 +8,68 @@ import {
   Submit as StyledSubmit,
   Error as StyledError,
   Redirect as StyledRedirect,
+  Info as StyledInfo,
 } from "../../styles/Form.style";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AUTH } from "../../config/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 type Inputs = {
   email: string;
   password: string;
 };
 
+type StatusType = {
+  error: boolean;
+  message: string;
+};
+
 export default function Login() {
+  const [status, setStatus] = useState<StatusType>({
+    error: false,
+    message: "",
+  });
+
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
 
-  const handleOnSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+  const handleOnSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      await signInWithEmailAndPassword(AUTH, data.email, data.password);
+
+      setStatus({
+        error: false,
+        message: "Login successful. You will be redirected shortly.",
+      });
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    } catch (err) {
+      setStatus({
+        error: true,
+        message: "Login failed.",
+      });
+    }
   };
 
   return (
     <StyledForm onSubmit={handleSubmit(handleOnSubmit)}>
       <StyledHeading>Login</StyledHeading>
+      {status.message.length > 1 && status.error ? (
+        <StyledError>{status.message}</StyledError>
+      ) : (
+        ""
+      )}
+      {status.message.length > 1 && !status.error ? (
+        <StyledInfo>{status.message}</StyledInfo>
+      ) : (
+        ""
+      )}
       <StyledLabel htmlFor="email">Email</StyledLabel>
       <StyledInput
         type="email"
